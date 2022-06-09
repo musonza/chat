53ecd519d0772ce29c3edd0eb512952900638168
inmanturbo <47095624+inmanturbo@users.noreply.github.com>
2022-06-08 20:11:55 -0400

    Update BaseModel.php

--- src/BaseModel.php
+++ src/BaseModel.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class BaseModel extends Model
 {
     protected $tablePrefix = 'chat_';
+    
+    public function getConnectionName()
+    {
+        return config('musonza_chat.database_connection', config('database.default'));
+    }
 }
